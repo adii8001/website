dd1bc0221192be5a14f81cb6b2dbb2a0d0632656
adii8001 <adnan.tech10@gmail.com>
2024-12-30 20:01:23 +0500

    added index.html

---
+++ comparision.js
@@ -0,0 +1,57 @@
+var product = [{
+        "name": "SeaGate",
+        "detail": "The best one touch from seagate",
+        "price": "12000",
+        "description": "with 1TB",
+        "brand": "SeaGate",
+        "image": "image/ONE TOUCH-700x550.jpg"
+    },
+    {
+        "name": "SeaGate drive",
+        "detail": "The best drive from seagate",
+        "price": "4000",
+        "description": "With 500gb Storage",
+        "brand": "SeaGate",
+        "image": "image/Seagate drive.webp"
+    },
+    {
+        "name": "LogiTech New Releases",
+        "detail": "Flagship killer from LogiTech",
+        "price": "8000",
+        "description": "with 1.5TB external harddrive",
+        "brand": "Logitech",
+        "image": "image/Logitec Releases New 1.5 TB External Hard Drives.jpg"
+    },
+    {
+        "name": "Samsung-Pc-harddrive",
+        "detail": "Flagship killer from Samsung",
+        "price": "4000",
+        "description": "With 2TB Storage",
+        "brand": "Samsung",
+        "image": "image/SAMSUNG-PC-harddrive.png"
+    },
+    {
+        "name": "Samsung Type-C Flash Drive",
+        "detail": "64gb\ 128gb\ Storage",
+        "price": "9000",
+        "description": "Launch with 1TB Storage",
+        "brand": "Logitech",
+        "image": "image/samsung_usb_type-c_flash_drive.jpg"
+    },
+    {
+        "name": "SeaGate",
+        "detail": "Feature-packed flagship from SeaGate",
+        "price": "8000",
+        "description": "with 6TB Storage",
+        "brand": "SeaGate",
+        "image": "image/Basic external hard drive seagate.png"
+    },
+    {
+        "name": "Samsung",
+        "detail": "Feature-packed flagship from Samsung",
+        "price": "8000",
+        "description": "With 512gb Storage SSD",
+        "brand": "Samsung",
+        "image": "image/Samsung SSD.jfif"
+    }
+]
